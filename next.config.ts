3b8cff757875ccbf2f06fa9f2248fc6d8370d9bd
tizianoforte <tiziano.forte@artsia.it>
2026-01-27 12:24:01 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/',
+                destination: '/login',
+            },
+        ]
+    },
+}
 
 export default nextConfig;
