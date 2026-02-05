2cd05021588e5764490550bcdba38262fdb0086a
tizianoforte <tiziano.forte@artsia.it>
2026-02-05 11:08:45 +0100

    add pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="it">
+        <html lang="it"><head>
+            <meta name="application-name" content="Artsia Dipendenti" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            <meta name="apple-mobile-web-app-title" content="Dipendenti" />
+            <meta name="description" content="Sistema gestione dipendenti Artsia" />
+            <meta name="format-detection" content="telephone=no" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="theme-color" content="#662D87" />
+
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+            <link rel="manifest" href="/manifest.json" />
+        </head>
         <body>{children}</body>
         </html>
     );
