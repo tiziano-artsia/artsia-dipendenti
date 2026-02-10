53b43594625377d00df6719a85ca7b8a1a52a9f2
tizianoforte <tiziano.forte@artsia.it>
2026-02-10 09:37:34 +0100

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -146,7 +146,7 @@ export default function Header() {
 
 
 
-            <header className="bg-white shadow-sm border-b border-gray-100 sticky top-0 z-40">
+            <header className="bg-white/70 backdrop-blur-md shadow-sm border-b border-gray-100/50 sticky top-0 z-40">
                 <div className="max-w-7xl mx-auto px-4 md:px-6 py-3 md:py-4">
                     <div className="flex items-center justify-between">
 
